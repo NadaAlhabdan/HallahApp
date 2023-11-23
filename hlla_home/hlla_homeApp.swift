2f90c047f54d783f4ec2b7c8fcd9fe4b73583743
NadaAlhabdan <NadaCs.alh@gmail.com>
2023-11-23 10:50:34 +0300

    Initial Commit

---
+++ hlla_home/hlla_homeApp.swift
@@ -0,0 +1,17 @@
+//
+//  hlla_homeApp.swift
+//  hlla_home
+//
+//  Created by Nada on 09/05/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct hlla_homeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
